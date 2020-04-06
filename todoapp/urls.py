f5becabe3c5705bd03afe94204406c2d226e0b48
olaniyi <ajayiolaniyi8@gmail.com>
2020-04-06 20:24:35 +0100

    delete is working after passing id

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.TodoListView.as_view(), name='todo'),
     path('new/', views.todoCreate, name='todo_create'),
     path('<int:todo_id>/delete/', views.todoDelete, name='todo_delete'),
+    path('<int:id>/update/', views.todoDelete, name='todo_update'),
     #path('new/', views.TodoCreateView.as_view(), name='todo_new'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -20,20 +20,33 @@ def todoCreate(request):
             todo = Todo()
             todo.text = todo_text
             todo.save()
-            #return redirect('todo')
+            return redirect('todo_create')
     else:
         form = TodoForm()
     return render(request, 'todoapp/todo_list.html', {'todo_list':qs, 'form':form})
     
     
-    
+
+def todoUpdate(request, id):
+    todo = Todo.objects.get(id=id)
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo)
+        if form.is_valid():
+            Todo(**form.cleaned_data)
+            todo.save()
+    else:
+        form = TodoForm(instance=todo)
+    return render(request, 'todoapp/todo_list.html', {'form':form})    
     
 
 
 def todoDelete(request, todo_id):
     todo = Todo.objects.get(id=todo_id)
     todo.delete()
-    return render(request, 'todoapp/todo_list.html', {'todo':todo})
+    return redirect('todo_create') #render(request, 'todoapp/todo_list.html', {'todo':todo})
+
+
+
 
 
 
