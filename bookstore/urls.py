ea6f31da8116347df3e5d49fa3e2cb625d01a392
olaniyi <ajayiolaniyi8@gmail.com>
2020-04-09 08:39:35 +0100

    detail generic view

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PublisherList
+from .views import PublisherList, PublisherDetail
 
 
 app_name = 'bookstore'
 urlpatterns = [
     path('publisher/', PublisherList.as_view(), name='publisher'),
+    path('publisher/<int:pk>/', PublisherDetail.as_view(), name='publisher_detail'),
 ]
--- bookstore/views.py
+++ bookstore/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from bookstore.models import Publisher
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from bookstore.models import Publisher, Book, Author
 
 
 class PublisherList(ListView):
     model = Publisher
+    context_object_name = 'publishers'
+
+
+class PublisherDetail(DetailView):
+    model = Publisher
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["books"] = Book.objects.all()
+        return context
+
+    
+    
